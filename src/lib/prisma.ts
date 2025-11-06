a8e07297aa8f6a43cc9e6b780b49d19277a78b91
limidoradevelopments-dev <limidora.developmets@gmail.com>
2025-11-06 15:59:14 +0530

    s

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import 'server-only';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../src/generated/client';
+
 
 // Prevent multiple client instances in Next.js dev (hot reload)
 declare global {
